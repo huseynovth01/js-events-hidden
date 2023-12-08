724d1bb39aae68cfefa35e42d4a20a44aa03e0f7
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-08 12:35:48 +0400

    task is done

--- js/about.js
+++ js/about.js
@@ -30,31 +30,47 @@ function openInformationCenter() {
 function closeInformationCenter() {
     information_center.classList.add("hidden");
     overlay.classList.add("hidden");
+    closeorder()
 }
 
 openButton.addEventListener("click", openInformationCenter);
 closeButton.addEventListener("click", closeInformationCenter);
 overlay.addEventListener("click", closeInformationCenter);
 
+document.addEventListener("keydown",function(event){
+    if (event.key==="Escape"){
+        closeInformationCenter();
+    }
+})
 
 
-var overlay = document.querySelector(".overlay");
-var information_center = document.querySelector(".information_center");
-var openButton = document.querySelector(".btn_phone");
-var closeButton = document.querySelector(".btn_x");
 
-function openInformationCenter() {
-    information_center.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-}
 
-function closeInformationCenter() {
+var overlay1 = document.querySelector(".overlay1");
+var order_call1 = document.querySelector(".order_call1");
+var order = document.querySelector(".order");
+var btn_cal  = document.querySelector("#btn-call")
+var btn_x_2 = document.querySelector("btn_x_2")
+
+function openorder(){
     information_center.classList.add("hidden");
-    overlay.classList.add("hidden");
+    order_call1.classList.remove("hidden1")
+    overlay.classList.add("hidden")
+    overlay1.classList.remove("hidden1")
+    
 }
 
-openButton.addEventListener("click", openInformationCenter);
-closeButton.addEventListener("click", closeInformationCenter);
-overlay.addEventListener("click", closeInformationCenter);
+function closeorder(){
+    order_call1.classList.add("hidden1")
+    overlay1.classList.add("hidden1")
+}
+
+// text_time.addEventListener("click",order_call1)
 
 
+
+overlay1.addEventListener("click", closeorder)
+
+// btn_x_2.addEventListener("click", closeorder)
+
+btn_cal.addEventListener("click", openorder)
